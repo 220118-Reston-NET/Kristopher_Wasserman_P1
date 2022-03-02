574712116004627d2ab1377c6c5831b194c08a3e
Kristopher Wasserman <kristopherwasserman6@msn.com>
2022-03-02 10:16:54 -0600

    fix

--- LakeJacksonCyclingModel/Orders.cs
+++ LakeJacksonCyclingModel/Orders.cs
@@ -7,92 +7,22 @@ namespace LakeJacksonCyclingModel
         public int orderID {get;set;}
         private string pIName;
         public int productID {get;set;}
-        public string pName 
-        {
-            get {return pIName; }
-            set
-            {
-                if(value != null)
-                {
-                    pIName = value;
-                }
-                else
-                {
-                    throw new ValidationException("Name must be included in product for order!");
-                }
-            }
-        }
-        private string address;
-        public string pAddress
-        {
-            get{ return address; }
-            set
-            {
-                if(address != null || address != "")
-                {
-                    address = value;
-                }
-                else
-                {
-                    throw new ValidationException("Store must have an address");
-                }
-            }
-        }
+        public string pName {get;set;}
+        
+        
+        public string pAddress{get;set;}
         public double Price {get;set;}
         public int storeid {get;set;}
 
-        private string sName;
-        public string storeName 
-        {
-            get{ return sName; }
-            set
-            {
-                if(value!= null)
-                {
-                    sName = value;
-                }
-                else
-                {
-                    throw new ValidationException("You must have a name");
-                }
-            }
-            
-        }
+        
+        public string storeName{get;set;}
 
-        private string SAddress;
-        public string StoreAddress 
-        {
-            get{return SAddress; }
-            set
-            {
-                if(value != null )
-                {
-                    SAddress = value;
-                }
-                else
-                {
-                    throw new ValidationException("Please add an address for the store");
-                }
-            }
-        }
+        public string StoreAddress{get;set} 
+        
         public decimal OrderTotal {get;set;}
 
-        private int q;
-        public int Quantity 
-        {
-            get { return q; }
-            set
-            {
-                if(value != 0)
-                {
-                    q = value;
-                }
-                else
-                {
-                    throw new ValidationException("Out-Of-Stock - Sorry");
-                }
-            }
-        }
+        public int Quantity{get;set;} 
+        
        
         public List<ItemLines> LineItems{get;set;}
 
